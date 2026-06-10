99be61a09d4e12474c536b9dfc3e25bef0d97aef
Kaeden Lee <leek117@unlv.nevada.edu>
2026-06-10 14:35:18 -0700

    Finish recipe generation file, update requirements.txt

---
+++ recipe_generation.py
@@ -0,0 +1,30 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+import os
+
+from apikey import gemini_api_key
+
+# API KEY
+os.environ["GEMINI_API_KEY"] = gemini_api_key
+
+# prompt for LLMChain
+template = """You are a chef who is extremely adaptable and capable when it comes to creating multilple types of dishes with just a few ingredients.
+Please create a recipe using the following ingredients: {ingredients}
+The recipe should include the name of the dish, a list of ingredients, step-by-step instructions, and the measurements."""
+
+prompt = PromptTemplate(template=template, input_variables=["ingredients"])
+
+#
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash", temperature=0.9, max_tokens=2048)
+
+
+recipe_chain = prompt | llm | StrOutputParser()
+
+
+ingredients = "chicken, rice, broccoli, soy sauce, garlic"
+
+recipe = recipe_chain.invoke({"ingredients": ingredients})
+
+print(recipe)
